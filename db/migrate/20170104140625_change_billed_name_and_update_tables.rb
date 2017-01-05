1872d2676b81995d99295ecfd42fb8c194eb4a11
Erik <flyers_rock_87@hotmail.com>
2017-01-05 01:14:54 +0000

    correct migrations

--- db/migrate/20170104140625_change_billed_name_and_update_tables.rb
+++ db/migrate/20170104140625_change_billed_name_and_update_tables.rb
@@ -1,7 +1,5 @@
 class ChangeBilledNameAndUpdateTables < ActiveRecord::Migration
   def change
-    rename_table :top_billeds, :people
-    drop_table :top_billed_listings
     remove_column :listings, :genre
     remove_column :listings, :top_billed
     add_column :people, :role, :string
