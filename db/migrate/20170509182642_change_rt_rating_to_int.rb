36f6e590967a4cc2090c5236973deae5a30c1b87
Erik <flyers_rock_87@hotmail.com>
2017-05-09 18:58:15 +0000

    hotfix in migration

--- db/migrate/20170509182642_change_rt_rating_to_int.rb
+++ db/migrate/20170509182642_change_rt_rating_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeRtRatingToInt < ActiveRecord::Migration
   def change
-    change_column :listings, :rt_rating, :integer
+    change_column :listings, :rt_rating, 'integer USING CAST(rt_rating AS integer)'
   end
 end
