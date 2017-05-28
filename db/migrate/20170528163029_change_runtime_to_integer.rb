7ea714e9a2c7ade89c93688a90b8ea17fb2cc465
Erik <flyers_rock_87@hotmail.com>
2017-05-28 16:35:40 +0000

    change runtime to integer

--- db/migrate/20170528163029_change_runtime_to_integer.rb
+++ db/migrate/20170528163029_change_runtime_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeRuntimeToInteger < ActiveRecord::Migration
   def change
-    change_column :listings, :runtime, :integer
+    change_column :listings, :runtime, 'integer USING CAST(runtime AS integer)'
   end
 end
