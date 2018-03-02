32f6fef1276405871c48500a37b2021e34ef900a
Erik <flyers_rock_87@hotmail.com>
2018-03-02 02:53:22 +0000

    test

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 #   config.assets.paths << path
 # end
 
-config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 
 # Add additional assets to the asset load path
