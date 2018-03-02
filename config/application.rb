dbcb93ea6484917476f55d50b3bb00d6723fddc0
Erik <flyers_rock_87@hotmail.com>
2018-03-02 02:41:23 +0000

    test

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module ErikGibbons
       config.assets.paths << path
     end
     config.assets.paths << Rails.root.join('node_modules')
-    config.assets.paths << Rails.root.join('node_modules/lightbox2')
+    config.assets.paths << Rails.root.join('node_modules/lightbox2/src/js')
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
