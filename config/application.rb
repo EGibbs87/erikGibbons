39909f797ce6fd5cc9a9b465f5ae2345780f4c53
Erik <flyers_rock_87@hotmail.com>
2018-03-02 02:28:39 +0000

    test

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ErikGibbons
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join('node_modules')
+    Dir.glob("#{Rails.root}/node_modules/**/").each do |path|
+      config.assets.paths << path
+    end
+    config.assets.paths << Rails.root.join('node_modules/*')
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
