f55c9c502a3659179daa4a1754f5fb50bd67b99a
Erik <flyers_rock_87@hotmail.com>
2018-06-12 18:53:46 +0000

    actionmailer bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root to: "application#angular"
   
-  post '/api/contact', 'application#contact'
+  post '/api/contact' => 'application#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
