8112a30b1ae3fd464706cd5cd1706a299b91603e
Erik <flyers_rock_87@hotmail.com>
2020-07-22 15:06:36 -0400

    fix cors multiple whitelist issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200', 'http://ng2-tv-charts.erikgibbons.com/'
+    origins 'localhost:4200' # development server
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch]
+  end
+
+  allow do
+    origins 'http://ng2-tv-charts.erikgibbons.com/' # production server
     resource '*', headers: :any, methods: [:get, :post, :put, :patch]
   end  
 end
