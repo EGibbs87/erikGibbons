5f24672f3b675fd57a555716513f3ce0f81443c9
Erik <flyers_rock_87@hotmail.com>
2026-04-11 10:45:51 -0400

    db fix

--- config/initializers/libsql_patch.rb
+++ config/initializers/libsql_patch.rb
@@ -1,7 +1,7 @@
-# Monkey-patch Libsql::Database to suppress auth_token leak from
-# turso_libsql Rust library's dbg! macro at src/lib.rs:281
+# Monkey-patches for turso_libsql and libsql_activerecord gems
 require 'libsql'
 
+# Fix 1: Suppress auth_token leak from Rust dbg! macro at src/lib.rs:281
 module Libsql
   class Database
     def initialize(options = {})
@@ -14,8 +14,6 @@ module Libsql
       desc[:sync_interval] = options[:sync_interval] || 0
       desc[:disable_read_your_writes] = !options.fetch(:read_your_writes, false)
 
-      # Suppress stderr during Database.init to prevent the Rust library
-      # from leaking auth_token via dbg! macro
       orig_stderr = STDERR.dup
       STDERR.reopen(File.open(File::NULL, 'w'))
       begin
@@ -28,6 +26,24 @@ module Libsql
       return unless block_given?
 
       begin yield self ensure close end
+    end
+  end
+end
+
+# Fix 2: pragma_table_list doesn't work over remote Hrana connections.
+# Override to use sqlite_master which works for both local and remote.
+module ActiveRecord
+  module ConnectionAdapters
+    class LibsqlAdapter < AbstractAdapter
+      def data_source_sql(name = nil, type: nil)
+        scope = quoted_scope(name, type:)
+        scope[:type] ||= "'table','view'"
+
+        sql = +"SELECT name FROM sqlite_master WHERE 1=1"
+        sql << " AND name = #{scope[:name]}" if scope[:name]
+        sql << " AND type IN (#{scope[:type]})"
+        sql
+      end
     end
   end
 end
